57484f3ab6d7ee80f101c5aec5fdaf18d4611298
hansnery <hansnery@gmail.com>
2019-12-31 14:37:29 -0300

    All buttons working

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -1,4 +1,7 @@
 const clockDisplay = document.getElementById("display");
+const pomodoroButton = document.getElementById("pomodoro");
+const quickBreakButton = document.getElementById("quick-break");
+const longBreakButton = document.getElementById("long-break");
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const resetButton = document.getElementById("reset");
@@ -14,44 +17,67 @@ function prependZero(number) {
     }
 }
 
-clockDisplay.innerHTML = prependZero(minutes) + ":" + prependZero(seconds);
-
-const timerGoes = function() {
-    if (seconds < 1 && minutes > 0) {
-        seconds = 59;
-        minutes--;
-    }   else {
-        seconds--;
-    }
+function updateDisplay() {
     clockDisplay.innerHTML = prependZero(minutes) + ":" + prependZero(seconds);
-    if (minutes == 0 && seconds == 0) {
-        stopTimer();
-    }
 }
 
-const timer = setInterval(()=> {
-    timerGoes();
-}, 1000);
+pomodoroButton.addEventListener("click", ()=> {
+    minutes = 24;
+    seconds = 0;
+    updateDisplay();
+});
 
-const stopTimer = ()=> {
-    clearInterval(timer);
-}
+quickBreakButton.addEventListener("click", ()=> {
+    minutes = 5;
+    seconds = 0;
+    updateDisplay();
+});
+
+longBreakButton.addEventListener("click", ()=> {
+    minutes = 10;
+    seconds = 0;
+    updateDisplay();
+});
+
+updateDisplay();
 
 startButton.addEventListener("click", ()=> {
+    const timerGoes = function() {
+        if (seconds < 1 && minutes > 0) {
+            seconds = 59;
+            minutes--;
+        }   else {
+            seconds--;
+        }
+        updateDisplay();
+        if (minutes == 0 && seconds == 0) {
+            stopTimer();
+        }
+    }
+
+    const timer = setInterval(()=> {
+        timerGoes();
+    }, 1000);
+    
+    const stopTimer = ()=> {
+        clearInterval(timer);
+    }
+
     if (minutes > 0 || seconds > 0) {
         timer;
     }   else {
         stopTimer();
     }
+
+    stopButton.addEventListener("click", ()=> {
+        stopTimer();
+    });
+    
+    resetButton.addEventListener("click", ()=> {
+        stopTimer();
+        minutes = 24;
+        seconds = 0;
+        clockDisplay.innerHTML = "24:00";
+    });
 });
 
-stopButton.addEventListener("click", ()=> {
-    stopTimer();
-});
-
-resetButton.addEventListener("click", ()=> {
-    stopTimer();
-    minutes = 24;
-    seconds = 0;
-    clockDisplay.innerHTML = "24:00";
-});
